67cc49a537ceac38f84cb3cba7865d03fb386858
Preston <32468763+PLHfor3@users.noreply.github.com>
2023-04-13 22:31:25 -0500

    Initial Setup

---
+++ tri-faith-application/MosqueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MosqueViewController.swift
+//  tri-faith-application
+//
+//  Created by Preston Higginbotham on 4/13/23.
+//
+
+import UIKit
+
+class MosqueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

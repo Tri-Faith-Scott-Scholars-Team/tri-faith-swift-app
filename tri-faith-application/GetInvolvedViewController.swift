524e5f07d0ff8cfbe4ebcc66eded992ee5eca367
Preston <32468763+PLHfor3@users.noreply.github.com>
2023-04-19 21:14:00 -0500

    Rewriting Home screen view controller and segue setup

--- tri-faith-application/GetInvolvedViewController.swift
+++ tri-faith-application/GetInvolvedViewController.swift
@@ -11,6 +11,7 @@ class GetInvolvedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
